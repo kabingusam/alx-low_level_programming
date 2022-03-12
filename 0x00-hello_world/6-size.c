c78aced42c6bc06a3c1e5758b724614b646c0dcb
kabingusam <kabingusammy@gmail.com>
2022-03-12 15:10:11 +0300

    size of various types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,13 +9,13 @@ int main() {
     char charType;
     int intType;
     long longType;
-    long int long intType;
+    long longintType;
     float floatType;
 
     printf("Size of chat: %zu bytes\n", sizeof(charType));
     printf("Size of int: %zu bytes\n", sizeof(intType));
     printf("Size of long: %zu bytes\n", sizeof(longType));
-    printf("Size of long int: %zu byte\n", sizeof(long intType));
+    printf("Size of long int: %zu byte\n", sizeof(longintType));
     printf("Size of float: %zu byte\n", sizeof(long floatType));
     return 0;
 }
