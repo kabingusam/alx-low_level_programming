66d809baf0002da598ae77488da681f5e5e99c91
sammy <80510478+kabingusam@users.noreply.github.com>
2022-03-12 03:13:32 -0800

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-/* This is the preferred style for multi-line
-* comments in C source code.
-* using printf.
-*/
-int main(void);
+/**
+ * main - Entry point
+ *
+ * using printf
+ */
+int main(void)
 {
         printf("with proper grammar, but the outcome is a piece of art,\n");
-        return(0);
-}       
+        return (0);
+}   
         
