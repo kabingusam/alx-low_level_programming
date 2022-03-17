170e38e802b840f7c04f800412926500b64eef06
kabingusam <kabingusammy@gmail.com>
2022-03-18 00:24:10 +0300

    task0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isupper - tests whether a character is a
+ * uppercase  letter from the English alphabet.
+ * @c: character to test.
+ * Return: 1 if it is a uppercase letter, 0 if it is not a uppercase letter.
+ */
+int _isupper(int c)
+{
+        if (c >= 'A' && c <= 'Z')
+                return (1);
+        else
+                return (0);
+}
