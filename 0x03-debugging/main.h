602168393d43d6c4095c0b8acffc225b142c02ef
kabingusam <kabingusammy@gmail.com>
2022-03-19 01:27:04 +0300

    hotfix

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,8 +1,29 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
 
-#include <stdio.h>
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
 
-void positive_or_negative(int i);
+int largest_number(int a, int b, int c)
+{
+    int largest;
 
-#endif /* MAIN_H */
+    if (a > b && b > c)
+    {
+        largest = a;
+    }
+    else if (b > a && a > c)
+    {
+        largest = b;
+    }
+    else
+    {
+        largest = c;
+    }
+
+    return (largest);
+}
