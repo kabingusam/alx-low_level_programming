b4cb74447f774ef631d46080d602f7b036ff06fa
kabingusam <kabingusammy@gmail.com>
2022-03-21 23:17:25 +0300

    hotfix

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,9 @@
 /**
-*main -takes a pointer to an int as parameter 
-*and updates the value it points to to 98.
-*@n - input to update to 98
-*
-*/
-void reset_to_98(int *n);
+ * reset_to_98 - updates value of pointer to 98.
+ * @n: input to update to 98.
+ */
+
+void reset_to_98(int *n)
 {
-    *n = 98;
+	*n = 98;
 }
