bb991899ed06b5b1f5251e69a8b88df1ccf46637
kabingusam <kabingusammy@gmail.com>
2022-03-22 00:53:00 +0300

    task3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts - prints a string, followed by a new line.
+ * @str: input string to print.
+ *main - prints a string, followed by a new line, to stdout
+ */
+ void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
