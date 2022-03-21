655773b8780a7c7539eadf98ad1847f8a56c0cb1
kabingusam <kabingusammy@gmail.com>
2022-03-21 23:43:10 +0300

    task1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+
+*main -Writes a function that swaps the values of two integers.
+* prototype -void swap_int(int *a, int *b);
+*@a - first value to swap
+*@b - second value to swap
+*/
+void swap_int(int *a, int *b);
+{
+    int temp;
+
+    temp = *a;
+    *a = b;
+    *b = temp; 
+}
+[A[C
