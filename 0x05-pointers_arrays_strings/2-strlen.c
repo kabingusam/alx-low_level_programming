fd3e1bd3417da3fc5fa79a9c917101fa47f7e787
kabingusam <kabingusammy@gmail.com>
2022-03-22 00:34:35 +0300

    hotfix

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,6 +1,8 @@
 /**
 *main - Write a function that returns the length of a string
 *Prototype: int _strlen(char *s);
+*_strlen(char *s) - calculates the length of a string
+*Return - retuns the length of s string
 *@s: input to print length of a string.
 */
  int _strlen(char *s)
