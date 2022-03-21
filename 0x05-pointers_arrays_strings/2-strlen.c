edcb1ffe2ae780c78503df4d819d0693fa71dd18
kabingusam <kabingusammy@gmail.com>
2022-03-22 00:43:38 +0300

    hotfix

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,17 +1,17 @@
 /**
-* _strlen(char *s) - calculates the length of a string.
-* @s: input to print length of a string.
-* Return - return the length of s string
-*/
- int _strlen(char *s)
- {
-     int i;
+ * _strlen - calculates the length of a string.
+ * @s: pointer to the character array.
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+	int i;
 
-     i = 0;
-     while(*s != '\0')
-     {
-         i++;
-         s++;
-     }
-     return(i);
- }
+	i = 0;
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	return (i);
+}
