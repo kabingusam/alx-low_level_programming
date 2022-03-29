546518162f475a366b01d7be1313e341618f6079
kabingusam <kabingusammy@gmail.com>
2022-03-30 01:18:33 +0300

    uplaod

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -4,21 +4,21 @@
  * print_chessboard - prints the chessboard
  * @a: the board to print
  */
-void print_chessboard(char (*a)[8])
+void print_chessboard(char (*a)[8])        
 {
-	int i, j;
+        int i, j;
 
-	i = j = 0;
-	while (i < 8)
-	{
-		j = 0;
-		while (j < 8)
-		{
-			_putchar(a[i][j]);
-			j++;
-		}
-		_putchar('\n');
-		i++;
-	}
+        i = j = 0;
+        while (i < 8)
+        {
+                j = 0;
+                while (j < 8)
+                {
+                        _putchar(a[i][j]); 
+                        j++;
+                }
+                _putchar('\n');
+                i++;
+        }
 
 }
