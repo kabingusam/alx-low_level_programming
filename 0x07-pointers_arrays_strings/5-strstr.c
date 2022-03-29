109a30d09d736a74128a3abaee4ef6afd93d9ffd
sammy <80510478+kabingusam@users.noreply.github.com>
2022-03-30 01:13:09 +0300

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,7 +8,7 @@
  * any other number if they are different.
  */
 int _strncmp(char *s1, char *s2, unsigned int b)
-
+{
 	unsigned int i;
 	for (i = 0; i <= b && *s1 == *s2 && *s1 != '\0' && *s2 != '\0'; i++)
 	{
