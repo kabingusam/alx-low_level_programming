9d2a5aa7a380d06b05627adab332cef2d9d3ae0c
kabingusam <kabingusammy@gmail.com>
2022-04-05 01:11:04 +0300

    uodate

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -7,23 +7,23 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *p;
-	int s1count, s2count, sizeBuffer, i;
+        char *p;
+        int s1count, s2count, sizeBuffer, i;
 
-	/*Check for valid strings*/
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	for (s1count = 0; s1[s1count]; s1count++)
-		;
-	for (s2count = 0; s2[s2count]; s2count++)
-		;
-	sizeBuffer = s1count + s2count + 1;
-	p = malloc(sizeBuffer * sizeof(char));
-	if (p == NULL)
-		return (NULL);
-	for (i = 0; i < sizeBuffer; i++)
-		i < s1count ? (p[i] = s1[i]) : (p[i] = s2[i - s1count]);
-	return (p);
+        /*Check for valid strings*/
+        if (s1 == NULL)
+                s1 = "";
+        if (s2 == NULL)
+                s2 = "";
+        for (s1count = 0; s1[s1count]; s1count++)      
+                ;
+        for (s2count = 0; s2[s2count]; s2count++)
+                ;
+        sizeBuffer = s1count + s2count + 1;
+        p = malloc(sizeBuffer * sizeof(char));
+        if (p == NULL)
+                return (NULL);
+        for (i = 0; i < sizeBuffer; i++)
+                i < s1count ? (p[i] = s1[i]) : (p[i] = s2[i - s1count]);
+        return (p);
 }
