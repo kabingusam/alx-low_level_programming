1919faba565016ff77e619a7c8fb88384ac9ea1d
sammy <80510478+kabingusam@users.noreply.github.com>
2022-04-25 17:08:56 +0300

    yet

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+* More singly linked list.
+* pop_listint : deletes the head node of a listint_t linked list
+*/
+int pop_listint(listint_t **head)
+{
+    listint_t *tmp;
+}
