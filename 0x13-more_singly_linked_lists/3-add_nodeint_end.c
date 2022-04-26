4bc180e2f01769a2828c7603bc06a70a0e720381
sammy <80510478+kabingusam@users.noreply.github.com>
2022-04-26 15:44:10 +0300

    update

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
 /**
 * More singly linked list.
@@ -5,48 +7,24 @@
 * @n: int to add to the list.
 * return : the  current postion in the list.
 */
+
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *new, *last;
+    listint_t *new , *present;
+    new = malloc(sizeof(listint_t));
 
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
+    if(new == NULL)
+        return(NULL);
+    new->n = n;
+    new->next = NULL;
 
-	new->n = n;
-	new->next = NULL;
-
-	if (*head == NULL)
-		*head = new;
-
-	else
-	{
-		last = *head;
-		while (last->next != NULL)
-			last = last->next;
-		last->next = new;
-	}
-
-	return (*head);
+    if (*head = NULL)
+    {
+        *head = new;
+        return(*head);
+    }
+    while(present->next != NULL)
+            present = present->next;
+        present->next = new;
+        return(new);
 }
-// listint_t *add_nodeint_end(listint_t **head, const int n)
-// {
-//     listint_t *new , *present;
-
-//     new = malloc(sizeof(listint_t));
-//     if(new == NULL)
-//         return(NULL);
-//     new->n = n;
-//     new->next = NULL;
-
-//     if (*head = NULL)
-//         *head = new;
-//     else
-//     {
-//         present = *head;
-//         while(present->next != NULL)
-//             present = present->next;
-//         present->next = new;
-//         return(*head);
-//     } 
-// }
