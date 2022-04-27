497db06527d5701ea34d52236d39f69e767533da
sammy <80510478+kabingusam@users.noreply.github.com>
2022-04-27 05:23:18 +0300

    tas6 update

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -4,8 +4,21 @@
 /**
 * More singly linked list.
 * pop_listint : deletes the head node of a listint_t linked list
+* @rem: what is to be returned.
 */
 int pop_listint(listint_t **head)
 {
     listint_t *tmp;
+	int rem;
+
+	if (*head == NULL)
+		return (0);
+
+	tmp = *head;
+	ret = (*head)->n;
+	*head = (*head)->next;
+
+	free(tmp);
+
+	return (rem);
 }
