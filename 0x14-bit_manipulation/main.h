e3ab33886cbdc77adf38df42ebd98069c1e4ed6f
sammy <80510478+kabingusam@users.noreply.github.com>
2022-04-28 15:12:42 +0300

    header update

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,3 +1,10 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+/**
+* header file with all the Prototypes.
+*/
+
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
@@ -5,3 +12,5 @@ int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
+
+#endif
