4be7f28f37a244cf1eaa513d141f5b276ef55678
sammy <80510478+kabingusam@users.noreply.github.com>
2022-04-28 15:43:44 +0300

    update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,21 +11,17 @@
 */
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int numb = 0;
-    mult = 1;
-    int len;
+  unsigned int num = 0;
+  int i = 0;
 
-    if(!b)
-        return(0);
-	for (len = 0; b[len];)
-		len++;
-	for (len -= 1; len >= 0; len--)
-	{
-		if (b[len] != '0' && b[len] != '1')
-			return (0);
+  if (b == NULL)
+    return 0;
 
-		numb += (b[len] - '0') * mult;
-		mult *= 2;
-	}
-	return (numb);
+  while (b[i] == '0' || b[i] == '1')
+  {  // Found another digit.
+    num <<= 1;
+    num += b[i]-'0';
+    i++;
+  }
+  return num;
 }
