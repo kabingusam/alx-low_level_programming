282e9276a22d71a5f467a7cbde4ef9d54e5dc71e
sammy <80510478+kabingusam@users.noreply.github.com>
2022-04-28 15:25:31 +0300

    task1

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -4,7 +4,7 @@
 /**
 * header file with all the Prototypes.
 */
-
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
