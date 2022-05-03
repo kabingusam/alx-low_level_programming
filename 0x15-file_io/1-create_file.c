b3e8c87fdbe2d95779cac17b6ffe6b174780f593
sammy <80510478+kabingusam@users.noreply.github.com>
2022-05-04 02:12:06 +0300

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,22 +11,22 @@
 * if filename is NULL return -1
 * if text_content is NULL create an empty file
 */
-
 int create_file(const char *filename, char *text_content)
 {
-    int o, w, len = 0;
+	int o, w, len = 0;
 
-    if(filename == NULL);
-        return(-1);
-    if(text_content == NULL)
-    {
-        for(len = 0;text_content[len];)
-            len++;
-    }
-    o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-    w = write(o, text_content ,len);
-    if(o == -1 || r == -1)
-        return(-1);
-    close(o);
-    return(1);
+	if (filename == NULL)
+		return (-1);
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
+		return (-1);
+	close(o);
+	return (1);
 }
