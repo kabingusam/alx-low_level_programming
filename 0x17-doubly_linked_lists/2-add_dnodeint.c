4d01180bf1ea528a8692d79081f3383c4c0db977
Kabingu Sammy <80510478+kabingusam@users.noreply.github.com>
2022-06-12 00:32:39 +1100

    Update 2-add_dnodeint.c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,1 +1,19 @@
-
+#include "lists.h"
+/**
+ * head: head of the linked list
+ * n : value to add to the list
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+    dlistint_t *newNode;
+    
+    newNode = *head;
+    newNode = malloc(sizeof(dlistint_t));
+    if(newNode == null)
+        return (Null);
+    newNode->n = n;
+    newNode->prev = Null;
+    newNode->next = *head;
+    *head = newNode;
+    return(newNode);
+}
