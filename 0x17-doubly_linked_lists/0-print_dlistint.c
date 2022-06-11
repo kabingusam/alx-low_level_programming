b652bf1ad3002a9becd5ba642e7e7664af134614
Kabingu Sammy <80510478+kabingusam@users.noreply.github.com>
2022-06-11 23:55:24 +1100

    task completion

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,1 +1,17 @@
+#include "list.h"
+/**
+ * print_dlistint: print the doubly linked list
+ * n : next integer
+ * return : number of items 
+ */
 
+size_t print_dlistint(const dlistint_t *h)
+{
+    size_t i;
+    for(i = 0; h; i++)
+    {
+        printf("%d\n",h->n);
+        h = h->next;
+    }
+    return(i);
+}
