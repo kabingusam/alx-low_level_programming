16b9a3bb581f03e937d2a8c210ef3fcb67a6d6d3
Kabingu Sammy <80510478+kabingusam@users.noreply.github.com>
2022-06-12 00:12:52 +1100

    Update 1-dlistint_len.c

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,1 +1,14 @@
-
+#include "lists.h"
+/**
+ * h : head of the list
+ * Return: the number of elements in a linked
+ * dlistint_len : gets length of the list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+    size_t i;
+    
+    for(i = 0; h; i++)
+        h = h->next;
+    return(i)
+}
