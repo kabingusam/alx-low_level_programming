32e344f714a94d4b73d8acc7376e6c9de5ec077d
Kabingu Sammy <80510478+kabingusam@users.noreply.github.com>
2022-06-13 16:00:24 +0300

    task5 ompletion

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,1 +1,16 @@
-
+#include "lists.h"
+/**
+ * fget_dnodeint_at_index - feturns the nth node of a dlistint_t linked list.
+ * head - head of the linked list
+ * unsigned int index : the index of the node, starting from 0
+ */
+ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+ {
+     for(;index != 0;index--)
+     {
+        if(head == NULL)
+            return(NULL);
+        head = head->next;
+     }
+     return(head)
+ }
