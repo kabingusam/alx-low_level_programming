6c237778d2558e5cc9edb7925e420bc1631a60dc
Kabingu Sammy <80510478+kabingusam@users.noreply.github.com>
2022-06-13 15:34:53 +0300

    task 4 completion

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,1 +1,18 @@
-
+#include "lists.h"
+/**
+ * free_dlistint - frees a linked list
+ * head - head of the linked list
+ */
+void free_dlistint(dlistint_t *head)
+{
+    dlistint_t *current;
+    
+    while(current)
+    {
+        current = head;
+        head->next = head;
+        current->prev = NULL;
+        current->next = NULL;
+        free(current);
+    }
+}
