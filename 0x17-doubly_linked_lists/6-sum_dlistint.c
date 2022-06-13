4cf7fb21bedea6144ed4b1fe0f5e275e62aef0d4
Kabingu Sammy <80510478+kabingusam@users.noreply.github.com>
2022-06-13 16:18:33 +0300

    task 6 completion

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,1 +1,16 @@
-
+#include "lists.h"
+/**
+ * sum_dlistint: returns sum of all the data (n) in a linked list.
+ * head: head of the list
+ */
+int sum_dlistint(dlistint_t *head)
+{
+    int sum;
+    
+    while(head)
+    {
+        sum += head->n;
+        head = head->next;
+    }
+    return(sum);
+}
